a3b28bd856e92b449f4c925fc400c994ffabdbc8
Brian Jordan <bcjordan@gmail.com>
2011-04-14 12:13:02 -0400

    add geokit, json

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MBTAAPI::Application.config.secret_token = '503bf7586eec67d753f274b8ac307b2683eecb3512bbbc5da972394bea563f8f1dd59bfcf604bb16cef02529007b1021e3d86d4efac56bcca3d3dc69ba231666'
