e165646fad200955dd631dd736d3b6f118b0aa6b
Brian Jordan <bcjordan@gmail.com>
2011-04-14 21:45:39 -0400

    add plugin/installed geokit, geocode stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,9 @@
 class Station < ActiveRecord::Base
+
+  acts_as_mappable :default_units => :miles,
+                           :default_formula => :sphere,
+                           :distance_field_name => :distance,
+                           :lat_column_name => :stop_lat,
+                           :lng_column_name => :stop_lon
+
 end
